eb1494da4e00da4db7088be6b7613e96aae496aa
Toffy0764 <ptoffanetto@gmail.com>
2026-08-01 14:45:55 +0200

    Add verifica_vwap function to compare spx and vwap

--- vwap.py
+++ vwap.py
@@ -1,1 +1,13 @@
+def verifica_vwap(spx, vwap):
 
+    if spx > vwap:
+        return {
+            "sopra_vwap": True,
+            "segnale": "Sopra VWAP"
+        }
+
+    else:
+        return {
+            "sopra_vwap": False,
+            "segnale": "Sotto VWAP"
+        }
