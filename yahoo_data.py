faabd392946032245daf0aa97ea46af503f1034d
Toffy0764 <ptoffanetto@gmail.com>
2026-08-02 19:55:29 +0200

    Versione 1.9.1 - Yahoo Finance Data Engine corretto

--- yahoo_data.py
+++ yahoo_data.py
@@ -1,16 +1,37 @@
 import yfinance as yf
+import pandas as pd
+
+
+TICKER = "^GSPC"
+
+
+def get_data():
+
+    dati = yf.download(
+        TICKER,
+        period="5d",
+        interval="15m",
+        progress=False,
+        auto_adjust=False
+    )
+
+    if dati.empty:
+        raise Exception(
+            "Nessun dato ricevuto da Yahoo Finance"
+        )
+
+    return dati
+
 
 
 def get_spx_price():
 
-    ticker = yf.Ticker("^GSPC")
-
-    dati = ticker.history(
-        period="1d",
-        interval="5m"
-    )
+    dati = get_data()
 
     ultimo = dati["Close"].iloc[-1]
+
+    if isinstance(ultimo, pd.Series):
+        ultimo = ultimo.iloc[0]
 
     return round(
         float(ultimo),
@@ -21,44 +42,52 @@ def get_spx_price():
 
 def get_intraday_prices():
 
-    ticker = yf.Ticker("^GSPC")
+    dati = get_data()
 
-    dati = ticker.history(
-        period="1d",
-        interval="5m"
-    )
+    prezzi = dati["Close"]
+
+    if isinstance(prezzi, pd.DataFrame):
+        prezzi = prezzi.iloc[:,0]
 
 
     prezzi = (
-        dati["Close"]
+        prezzi
         .dropna()
         .tolist()
     )
 
 
-    return prezzi
+    if len(prezzi) == 0:
+        raise Exception(
+            "Serie prezzi vuota"
+        )
+
+
+    return [
+        round(float(x),2)
+        for x in prezzi
+    ]
 
 
 
 def get_vwap():
 
-    ticker = yf.Ticker("^GSPC")
+    dati = get_data()
 
 
-    dati = ticker.history(
-        period="1d",
-        interval="5m"
-    )
-
-
-    prezzo = dati["Close"]
-
+    close = dati["Close"]
     volume = dati["Volume"]
 
 
+    if isinstance(close, pd.DataFrame):
+        close = close.iloc[:,0]
+
+    if isinstance(volume, pd.DataFrame):
+        volume = volume.iloc[:,0]
+
+
     vwap = (
-        (prezzo * volume)
-        .sum()
+        (close * volume).sum()
         /
         volume.sum()
     )
